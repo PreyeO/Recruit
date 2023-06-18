8b0509d4097610a8d5f34faa8b945451e4b94dbf
Osunkoya Jubril <osunkoyajubril6000@gmail.com>
2023-06-18 12:17:58 +0100

    verify details page added

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import './App.css'
-import GlobalStyles from './Components/Styles/General/GlobalStyles'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Landing from './Pages/LandingPage/Landing'
-import SignIn from './Pages/Authentication/SignIn'
-import SignUp from './Pages/Authentication/SignUp'
+// import "./App.css";
+import GlobalStyles from "./Components/Styles/General/GlobalStyles";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Landing from "./Pages/LandingPage/Landing";
+import SignIn from "./Pages/Authentication/SignIn";
+import SignUp from "./Pages/Authentication/SignUp";
+import VerifyDetails from "./Pages/Authentication/VerifyDetails";
+// import CompleteProfile from './Pages/Authentication/SignUp'
+// import AddSocials from './Pages/Authentication/AddSocials'
 
 function App() {
- 
-
   return (
     <>
-    <GlobalStyles />
-    <BrowserRouter>
-     <Routes>
-      <Route path="/" element={<Landing />} />
-      <Route path="/signin" element={<SignIn />} />
-      <Route path="/signup" element={<SignUp />} />
-     </Routes>
-     </BrowserRouter>
-     </>
-  
-  )
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/verifydetails" element={<VerifyDetails />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
-export default App
+export default App;
