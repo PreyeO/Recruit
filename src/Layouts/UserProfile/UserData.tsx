01d6ed1e3e7b3615f770a0f94615f8c58a26c3c9
preye omusuku <omusukup@yahoo.com>
2023-07-22 21:55:26 +0100

    add proficiencies

--- src/Layouts/UserProfile/UserData.tsx
+++ src/Layouts/UserProfile/UserData.tsx
@@ -1,7 +1,29 @@
+import {
+  RiReactjsFill,
+  RiVuejsFill,
+  RiHtml5Fill,
+  RiCss3Fill,
+  RiAngularjsFill,
+  RiCodeBoxFill,
+} from "react-icons/ri";
+import { AiFillGithub, AiFillGitlab } from "react-icons/ai";
+import { BsBootstrap } from "react-icons/bs";
+
 export const datas = [
   {
     id: 1,
-    skills: ["REACT, VUE3, JAVASCRIPT, TAILWIND, HTML5"],
-    tools: ["Firebase, Github, VS Code"],
+    skills: [
+      { title: "REACT", icon: <RiReactjsFill /> },
+      { title: "VUE3", icon: <RiVuejsFill /> },
+      { title: "CSS", icon: <RiCss3Fill /> },
+      { title: "Angular", icon: <RiAngularjsFill /> },
+      { title: "HTML5", icon: <RiHtml5Fill /> },
+    ],
+    tools: [
+      { title: "Coder Box", icon: <RiCodeBoxFill /> },
+      { title: "GitLab", icon: <AiFillGitlab /> },
+      { title: "GitHub", icon: <AiFillGithub /> },
+      { title: "Bootstrap", icon: <BsBootstrap /> },
+    ],
   },
 ];
--- src/Layouts/UserProfile/UserProficiencies.tsx
+++ src/Layouts/UserProfile/UserProficiencies.tsx
@@ -4,24 +4,35 @@ const UserProficiencies = () => {
   return (
     <div>
       {datas.map((data) => (
-        <div key={data.id} className="flex-col">
-          <h2>Skills</h2>
-          <ul>
-            {data.skills[0].split(", ").map((skill, index) => (
-              <li key={index} className="text-white bg-indigo-500 ">
-                {skill}
-              </li>
-            ))}
-          </ul>
-
-          <h2>Tools</h2>
-          <ul>
-            {data.tools[0].split(", ").map((tool, index) => (
-              <li key={index} className="text-white bg-orange-500 ">
-                {tool}
-              </li>
-            ))}
-          </ul>
+        <div key={data.id} className="flex flex-col justify-center gap-12">
+          <div>
+            <h2>Skills</h2>
+            <ul>
+              {data.skills.map((skill, index) => (
+                <li
+                  key={index}
+                  className="text-white bg-blue-500 flex items-center gap-2"
+                >
+                  {skill.icon}
+                  {skill.title}
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div>
+            <h2>Tools</h2>
+            <ul>
+              {data.tools.map((tool, index) => (
+                <li
+                  key={index}
+                  className="text-white bg-orange-500 flex items-center gap-2"
+                >
+                  {tool.icon}
+                  {tool.title}
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       ))}
     </div>
