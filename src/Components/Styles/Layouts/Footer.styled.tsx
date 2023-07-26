7552f1b7db78e46c821ebfabea5a53e8454aa692
Osunkoya Jubril <osunkoyajubril6000@gmail.com>
2023-07-26 12:39:56 +0100

    edited footer

--- src/Components/Styles/Layouts/Footer.styled.tsx
+++ src/Components/Styles/Layouts/Footer.styled.tsx
@@ -2,36 +2,10 @@ import styled from "styled-components";
 
 export const StyledFooter = styled.div`
   color: white;
-  .first_container {
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-    padding-top: 1rem;
-    padding-bottom: 3rem;
-    margin-top: 1rem;
-  }
-  .footer_btn {
-    flex-direction: column;
-  }
-  .horizontal_line {
-    width: 100%;
-    height: 1px;
-    bottom: -0.5px;
-    background: #8e9faa;
-  }
-  .footer_links {
-    display: flex;
-    justify-content: space-around;
-    padding-top: 1rem;
-    align-items: center;
-  }
-  .footer_logo {
-    flex-direction: column;
-  }
-  p {
-    width: 20rem;
-    padding-bottom: 1rem;
-  }
+  font-family: "Plus Jakarta Sans", sans-serif;
+  margin: 0 auto;
+  width: 90%;
+  
   ul {
     display: flex;
     flex-direction: column;
