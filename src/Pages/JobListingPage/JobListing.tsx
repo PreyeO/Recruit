6d98e22a8de0dae8eef1d18aab7db75aaf0a59f3
preye omusuku <omusukup@yahoo.com>
2023-08-24 23:26:36 +0100

    add search input

--- src/Pages/JobListingPage/JobListing.tsx
+++ src/Pages/JobListingPage/JobListing.tsx
@@ -13,7 +13,7 @@ const Profile = () => {
       <NavBar />
       <ProfileContainer>
         <div className="flex justify-between mb-10 px-6 pt-10">
-          <Search placeholder="Search" type="search" />
+          <Search placeholder="Search job title" type="search" />
           <SalaryRange min={minSalary} max={maxSalary} />
         </div>
         <div className="absolute h-[1px] left-0 bg-[#b5c2cb] w-full"></div>
